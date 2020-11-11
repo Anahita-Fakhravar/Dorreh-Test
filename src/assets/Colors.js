1887ad794cddfabc3417004390f07ab4d2556bba
ASUS <anahita_fakhravar@yahoo.com>
2020-11-11 08:24:01 +0330

    Define project color

---
+++ src/assets/Colors.js
@@ -0,0 +1,11 @@
+//project color definition
+
+export const Colors = {
+
+    darkTheme:'#b5042a',
+    lightTheme:'#e2e7dc',
+    mediumTheme:'#f5dc5f',
+    listColorArray:['#cbe7f7','#fffca3','#cdffab','#fcc7e0'],
+    txtOnLightTheme:'#1f0107'
+
+};
