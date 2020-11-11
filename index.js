65caa05421f1690c50738bc7d292d39a85cb8d9d
ASUS <anahita_fakhravar@yahoo.com>
2020-11-11 08:23:45 +0330

    Register UserListMain screen

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import USerListMain from './src/screens/users/UserListMain'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => USerListMain);
