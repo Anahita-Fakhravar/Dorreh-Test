e4a2e1110b6d85c9e131fec7d1ee0d6b62ab93a8
ASUS <anahita_fakhravar@yahoo.com>
2020-11-11 08:24:24 +0330

    Design custom topBar

---
+++ src/components/CustomTopBar.js
@@ -0,0 +1,28 @@
+//Design custom topbar 
+
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+import {Colors} from '../assets/Colors';
+import { responsiveHeight} from 'react-native-responsive-dimensions';
+export const CustomTopBar = ({ title }) => {
+    
+    return (
+        <View style={styles.topBarView}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    topBarView:{
+        backgroundColor:Colors.darkTheme,
+        height: responsiveHeight(10) ,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    title:{
+        color:Colors.lightTheme,
+        fontSize: 16,
+    }
+})
